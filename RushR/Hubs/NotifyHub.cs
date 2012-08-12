0922d7cb293a951a41927efcf545ad42af345d01
Nick Josevski <nickjosevski@gmail.com>
2012-08-12 15:20:08 +1000

    ignoring .net bundles and just adding signalR direct

---
+++ RushR/Hubs/NotifyHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using SignalR;
+using SignalR.Hubs;
+
+namespace RushR.Hubs
+{
+    [HubName("notifyHub")]
+    public class NotifyHub : Hub
+    {
+        public void Broadcast(string message)
+        {
+            Clients.alternateBroadcast(message);
+            GetClients().broadcast(message);
+        }
+
+        private dynamic GetClients()
+        {
+            return GlobalHost.ConnectionManager.GetHubContext<NotifyHub>().Clients;
+        }
+    }
+}
---
+++ steps.md
@@ -0,0 +1,12 @@
+### SignalR app demo steps
+
+ 1. File New Project
+ 2. Add SignalR
+ 3. Add hubs folder
+ 4. Create NotifyHub
+   - Broadcast method  
+ 5. Configure JS
+   - RegisterBundles
+   - src="/signalr/hubs"
+  6. Get past the bundle configuration as part of ASP.NET now, didn't like to deal with jquery.signalR-
+  7. 
